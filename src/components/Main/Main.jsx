2922c132d815271a7534c259606df98817548e50
Ormon-huehuehue <armnvrma10@gmail.com>
2024-06-07 16:56:23 +0530

    queries are now working with text animations

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./Main.css"
+import { useContext } from 'react'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context';
 
 const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context);
+
+
   return (
     <div className="main">
         <div className="nav">
@@ -12,6 +18,9 @@ const Main = () => {
 
       
         <div className="main-container">
+
+          {!showResult 
+          ? <>
           <div className="greet">
             <p><span> Hello, Armaan</span></p>
             <p>How can i help you today?</p>
@@ -38,14 +47,38 @@ const Main = () => {
               <img src={assets.message_icon}/>
             </div>
           </div>
+          </> 
+          :<>
+            <div className = "result">
+              <div className = "resultTitle">
+                <img src={assets.user_icon}/>
+                <p>{recentPrompt}</p>
+              </div>
+              <div className = "resultData">
+                <img src={assets.gemini_icon}/>
+                {loading
+                ? <div className="loader">
+                    <hr/>
+                    <hr/>
+                    <hr/>
+                </div>  
+                : <div>
+                  <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                </div>
+                }
+              </div>
+            </div>
+          </>
+          }
+
           
           <div className="mainBottom">
             <div className="searchBox">
-              <input type="text" placeholder="Enter a prompt here"/>
+              <input onChange={(e)=>setInput(e.target.value)} value ={input} type="text" placeholder="Enter a prompt here"/>
               <div>
                 <img src={assets.gallery_icon} alt="" />
                 <img src={assets.mic_icon} alt="" />
-                <img src={assets.send_icon} alt="" />
+                <img onClick={()=>onSent()} src={assets.send_icon} alt="" />
               </div>
             </div>
             <div>
